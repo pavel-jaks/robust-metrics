68f4e7a59b2dadd15ac13f8d2b4fe0ccd810219b
Pavel Jakš <stanio250@gmail.com>
2024-03-25 11:09:58 +0100

    Metrics time benchmark

---
+++ code/benchmarking_script.py
@@ -0,0 +1,59 @@
+import timeit
+
+import torch
+
+import metrics
+
+
+metric_wasserstein = metrics.WassersteinApproximation(regularization=3)
+metric_dssim5 = metrics.StructuralDissimilarity(window_size=5)
+metric_dssim10 = metrics.StructuralDissimilarity(window_size=10)
+metric_dssim20 = metrics.StructuralDissimilarity(window_size=20)
+metric_dssim100 = metrics.StructuralDissimilarity()
+metric_l2 = metrics.L2Metric()
+metric_l1 = metrics.LpMetric(1)
+metric_l0 = metrics.L0Metric()
+metric_linf = metrics.LinfMetric()
+metric_npsr = metrics.NoiseToPeakSignalRatio()
+
+
+def benchmark(metric, image_pair, number_of_runs, description):
+    setup = f"from __main__ import {metric}; import torch; image1 = {image_pair[0]}; image2 = {image_pair[1]};"
+    code = f"{metric}(image1, image2)"
+    time = timeit.repeat(stmt=code, setup=setup, repeat=1, number=number_of_runs)
+    print(description)
+    print(f"Metric: {metric}")
+    for i, result in enumerate(time):
+        print(f"Run {i + 1}: {result}")
+    print("_____________")
+
+
+def main():
+    image_pairs = [
+        ('torch.rand(10, 1, 28, 28)', 'torch.rand(10, 1, 28, 28)'),
+        ('torch.rand(100, 1, 28, 28)', 'torch.rand(100, 1, 28, 28)'),
+        ('torch.rand(1000, 1, 28, 28)', 'torch.rand(1000, 1, 28, 28)')
+    ]
+    for i, pair in enumerate(image_pairs):
+        benchmark(f'{metric_l2=}'.split('=')[0], pair, 10, f"test_images{i + 1}")
+    for i, pair in enumerate(image_pairs):
+        benchmark(f'{metric_dssim5=}'.split('=')[0], pair, 10, f"test_images{i + 1}")
+    for i, pair in enumerate(image_pairs):
+        benchmark(f'{metric_dssim10=}'.split('=')[0], pair, 10, f"test_images{i + 1}")
+    for i, pair in enumerate(image_pairs):
+        benchmark(f'{metric_dssim20=}'.split('=')[0], pair, 10, f"test_images{i + 1}")
+    for i, pair in enumerate(image_pairs):
+        benchmark(f'{metric_dssim100=}'.split('=')[0], pair, 10, f"test_images{i + 1}")
+    for i, pair in enumerate(image_pairs):
+        benchmark(f'{metric_l1=}'.split('=')[0], pair, 10, f"test_images{i + 1}")
+    for i, pair in enumerate(image_pairs):
+        benchmark(f'{metric_l0=}'.split('=')[0], pair, 10, f"test_images{i + 1}")
+    for i, pair in enumerate(image_pairs):
+        benchmark(f'{metric_linf=}'.split('=')[0], pair, 10, f"test_images{i + 1}")
+    for i, pair in enumerate(image_pairs):
+        benchmark(f'{metric_wasserstein=}'.split('=')[0], pair, 10, f"test_images{i + 1}")
+
+
+
+if __name__ == '__main__':
+    main()
--- code/tests/__init__.py
+++ code/tests/__init__.py
@@ -1,3 +1,4 @@
 # ...
 
 from tests.test_metrics import *
+from tests.benchmark import main as benchmark
